3d038174619246594b7106bdef3d1ecd3fc9a688
theanaskhxn <100487024+Cryptonictech@users.noreply.github.com>
2022-10-03 14:45:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,14 +23,14 @@ if(isset($_POST['enter'])){
  
 function loginForm(){
     echo
-    '<div id="loginform">
+  <div id="loginform">
     <p>Please enter your name to continue!</p>
     <form action="index.php" method="post">
       <label for="name">Name &mdash;</label>
       <input type="text" name="name" id="name" />
       <input type="submit" name="enter" id="enter" value="Enter" />
     </form>
-  </div>';
+  </div>;
 }
  
 ?>
